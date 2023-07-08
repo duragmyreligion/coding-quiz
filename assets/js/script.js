37ed18dd9c4007303d6f44fba04f4ef5f1f89459
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-07-09 00:03:42 +0800

    Made changes to script.js file and highscores.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,29 @@ const countdown = document.querySelector(".timer .countdown");
 
 start_btn.addEventListener('click', ()=>{
     main_page.remove();
+    showQuestions(0);
+    startTimer(75);
 });
+
+let timeValue = 75;
+let userScore = 0;
+let counter;
+let counterLine;
+
+function showQuestions(index) {
+    const que_text = document.querySelector(".question-txt");
+
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let choice_tag = '<div class="choice"><span>'+ questions[index].choices[0] +'</span></div>'
+    + '<div class="choice"><span>'+ questions[index].choices[1] +'</span></div>'
+    + '<div class="choice"><span>'+ questions[index].choices[2] +'</span></div>'
+    + '<div class="choice"><span>'+ questions[index].choices[3] +'</span></div>';
+    que_text.innerHTML = que_tag;
+    options.innerHTML = choice_tag;
+
+    const choice = options.querySelectorAll(".choice");
+
+    for(i=0; i < choice.length; i++) {
+        choice[i].setAttribute("onclick", "choiceSelected(this)");
+    }
+}
